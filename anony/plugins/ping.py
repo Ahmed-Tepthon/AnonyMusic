208b0903716fc59ff6ec441d247bc8a8d221b301
Ahmed Mido <ahmedmidonew@outlook.com>
2025-12-28 16:37:55 +0200

    Update ping.py
    Signed-off-by: Ahmed Mido <ahmedmidonew@outlook.com>

--- anony/plugins/ping.py
+++ anony/plugins/ping.py
@@ -2,7 +2,6 @@
 # Licensed under the MIT License.
 # This file is part of AnonXMusic
 
-
 import time
 import psutil
 
@@ -11,14 +10,21 @@ from anony import app, anon, boot, config, lang
 from anony.helpers import buttons
 
 
-filters.command(["ping", "البنج"], prefixes=["/", ""]) & ~app.bl_users)
+@app.on_message(
+    filters.command(["ping", "البنج"], prefixes=["/", ""]) & ~app.bl_users
+)
 @lang.language()
 async def _ping(_, m: types.Message):
     start = time.time()
     sent = await m.reply_text(m.lang["pinging"])
-    get_time = lambda s: (lambda r: (f"{r[-1]}, " if r[-1][:-4] != "0" else "") + ":".join(reversed(r[:-1])))([f"{v}{u}" for v, u in zip([s%60, (s//60)%60, (s//3600)%24, s//86400], ["s", "m", "h", "days"])])
+
+    get_time = lambda s: (
+        lambda r: (f"{r[-1]}, " if r[-1][:-4] != "0" else "") + ":".join(reversed(r[:-1]))
+    )([f"{v}{u}" for v, u in zip([s % 60, (s // 60) % 60, (s // 3600) % 24, s // 86400], ["s", "m", "h", "days"])])
+
     uptime = get_time(int(time.time() - boot))
     latency = round((time.time() - start) * 1000, 2)
+
     await sent.edit_media(
         media=types.InputMediaPhoto(
             media=config.PING_IMG,
