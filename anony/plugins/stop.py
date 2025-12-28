17f65b8537f800ce9e61a0e917477201684bf43f
Ahmed Mido <ahmedmidonew@outlook.com>
2025-12-28 16:10:36 +0200

    Update stop.py
    Signed-off-by: Ahmed Mido <ahmedmidonew@outlook.com>

--- anony/plugins/stop.py
+++ anony/plugins/stop.py
@@ -1,20 +1,17 @@
-# Copyright (c) 2025 AnonymousX1025
-# Licensed under the MIT License.
-# This file is part of AnonXMusic
-
-
 from pyrogram import filters, types
 
 from anony import anon, app, db, lang
 from anony.helpers import can_manage_vc
 
 
-@app.on_message(filters.command(["ايقاف", "وقف"]) & filters.group & ~app.bl_users)
+@app.on_message(
+    filters.command(["ايقاف", "وقف"], prefixes=["/", ""])  # ← هنا بدون /
+    & filters.group
+    & ~app.bl_users
+)
 @lang.language()
 @can_manage_vc
 async def _stop(_, m: types.Message):
-    if len(m.command) > 1:
-        return
     if not await db.get_call(m.chat.id):
         return await m.reply_text(m.lang["not_playing"])
 
