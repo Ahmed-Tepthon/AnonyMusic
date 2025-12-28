5f15733be1f6d797263a75b1e3a22431772177fb
Ahmed Mido <ahmedmidonew@outlook.com>
2025-12-28 15:02:48 +0200

    Update play.py
    Signed-off-by: Ahmed Mido <ahmedmidonew@outlook.com>

--- anony/plugins/play.py
+++ anony/plugins/play.py
@@ -1,6 +1,6 @@
-"""
-Copyright (c) 2025 AnonymousX1025
-"""
+# Copyright (c) 2025 AnonymousX1025
+# Licensed under the MIT License.
+# This file is part of AnonXMusic
 
 Licensed under the MIT License.
 
